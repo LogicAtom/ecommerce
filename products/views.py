a0acbbb310e239debb493d0ff6ce0e7edfd1e3f8
LogicAtom <anthonykoz5909@gmail.com>
2021-11-13 23:54:56 +0000

    views.py>Pep8 Compliance>Fixed 8 errors

--- products/views.py
+++ products/views.py
@@ -5,22 +5,27 @@ from .models import Product
 
 # Create your views here.
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    query = None # Start with None to ensure no error when searching with no search term
-# Check if request.get exists, if 'q' is in request then set it as a request called query
-    category = None # Capture the category parameter
+    # Start with None to ensure no error when searching with no search term
+    query = None
+# Check if request.get exists, if 'q' is
+# in request then set it as a request called query
+    category = None  # Capture the category parameter
     if request.GET:
 
         if 'q' in request.GET:
             query = request.GET['q']
-            if not query: # if query is blank then it wont return any results
-                messages.error(request, "You didn't enter any search criteria!")
+            if not query:  # if query is blank then it wont return any results
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             # Constructing queries
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             # Pass queries to a filter
             products = products.filter(queries)
 

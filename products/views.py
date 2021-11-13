7159cec7aefbc1983f918049275de52e90acf9bd
LogicAtom <anthonykoz5909@gmail.com>
2021-11-13 21:30:32 +0000

    views.py>build code for checking for request.get and give custom validation error message

--- products/views.py
+++ products/views.py
@@ -7,6 +7,13 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+# Check if request.get exists, if 'q' is in request then set it as a request called query
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query: # if query is blank then it wont return any results
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
 
     context = {
         'products': products,

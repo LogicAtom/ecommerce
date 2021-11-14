bfd0d3a8b3d62dc57e45a3da91772291cd0d5fe1
LogicAtom <anthonykoz5909@gmail.com>
2021-11-14 00:04:19 +0000

    views.py, import Category, and, create>if request.GET for if 'category'

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -15,7 +15,12 @@ def all_products(request):
 # Check if request.get exists, if 'q' is
 # in request then set it as a request called query
     category = None  # Capture the category parameter
+
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']

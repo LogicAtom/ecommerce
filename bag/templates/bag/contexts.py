f2cc3542ef54feee47e5a5e0c364596af44a887a
LogicAtom <anthonykoz5909@gmail.com>
2021-11-16 18:32:12 +0000

    contexts.py, items added to context so everything is available to every template in every app across the entire project

--- bag/templates/bag/contexts.py
+++ bag/templates/bag/contexts.py
@@ -10,7 +10,7 @@ def bag_contents(request):
     product_count = 0  # initialize to 0
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
@@ -18,6 +18,14 @@ def bag_contents(request):
 
     grand_total = delivery + total
 
-    context = {}
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
 
     return context

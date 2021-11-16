7a6d33dcbea7b456e99e3d4b883a5f0ad3c88a06
LogicAtom <anthonykoz5909@gmail.com>
2021-11-16 18:25:37 +0000

    contexts.py> else statement built., and, created math for grand_total

--- bag/templates/bag/contexts.py
+++ bag/templates/bag/contexts.py
@@ -12,6 +12,11 @@ def bag_contents(request):
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
 
     context = {}
 

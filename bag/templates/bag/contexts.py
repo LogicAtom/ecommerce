59b83014a315493f99efa66ddb0d2cb1eff9059f
LogicAtom <anthonykoz5909@gmail.com>
2021-11-16 18:21:38 +0000

    contexts.py, create math to handle delivery costs., add imports

--- bag/templates/bag/contexts.py
+++ bag/templates/bag/contexts.py
@@ -1,3 +1,6 @@
+# decimal is accurate, do not use float because it rounds off
+from decimal import Decimal
+from django.conf import settings
 
 
 def bag_contents(request):
@@ -5,6 +8,9 @@ def bag_contents(request):
     bag_items = []
     total = 0  # initialize to 0
     product_count = 0  # initialize to 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
 
     context = {}
 

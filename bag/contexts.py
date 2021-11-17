d55c7d7f92ab1d955404de23faf493791d9b4115
LogicAtom <anthonykoz5909@gmail.com>
2021-11-17 03:09:45 +0000

    bag>views.py, create def add_to_bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,7 +10,7 @@ def bag_contents(request):
     bag_items = []
     total = 0  # initialize to 0
     product_count = 0  # initialize to 0
-    bag = request.session.get('bag', ())
+    bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
--- bag/views.py
+++ bag/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
-
-
-def view_bag(request):
-    """ A view that renders the bag contents page """
-
-    return render(request, 'bag/bag.html')
 
 
 def view_bag(request):
@@ -19,14 +18,13 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     product = get_object_or_404(Product, pk=item_id)
-    # Convert to integer from string
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-    # Variable bag, accesses the session, get it into a bag if it exists, or create one if it doesnt exist
     bag = request.session.get('bag', {})
+
     if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
@@ -52,10 +50,8 @@ def add_to_bag(request, item_id):
                              (f'Updated {product.name} '
                               f'quantity to {bag[item_id]}'))
         else:
-            # If there is already a key for the dictionary item, then increment it accordingly
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
-    print(request.session['bag'])  # Test session
     return redirect(redirect_url)

c73ee30de8ea44437df5324c0c9ea6fed1e67a5a
LogicAtom <anthonykoz5909@gmail.com>
2021-12-03 02:43:40 +0000

    checkout, views.py, print(intent)

--- checkout/views.py
+++ checkout/views.py
@@ -9,17 +9,31 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K2MUSI9ST2KCxBEW8rQuyFvAb7mO1F66C3VEq1fr7yQKcabeI6CLaZPCQ3wxHW7gicHX2jd7g5YSzwXDaktiKJc00ThfGy1FI',
-        'client_secret': 'test client secret',
+        'stripe_public_key': 'stripe_public_key',
+        'client_secret': 'client_secret',
     }
 
     return render(request, template, context)
